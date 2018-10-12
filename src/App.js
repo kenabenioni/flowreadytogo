8f677b7d8858ac5b7b0bdc3310ca610983cfa597
Kena Benioni <kenabenioni@Kenas-MacBook-Pro.local>
2018-10-12 11:45:02 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+// import React, { Component } from 'react';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// class App extends Component<{}> {
+//   render() {
+//     return (
+//       <div className="App">
+//          <h1>Flow Ready To Go!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+function area (r) {
+  return Math.PI * r * r
 }
 
-export default App;
+const result = area("1O")
+
+// console.log(result);
+
